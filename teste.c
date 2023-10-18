03b41705551d3b2e805ddd08ccc2537ec315b4e0
Caio R <caiocleo1@gmail.com>
2023-10-18 01:18:18 -0300

    Create teste.c

---
+++ teste.c
@@ -0,0 +1,13 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void);
+
+    int main(void)
+    {
+        int a=0;
+        for (;a<100000;)
+            printf(" %d,",a++);
+        return 0;
+    }
